c5498f454675f11163ec684ae61b8a9f8b07e4ba
nshorty <andrysciortino@gmail.com>
2021-02-05 07:55:21 +0100

    tulip: update to Redfin's February FP

--- properties.mk
+++ properties.mk
@@ -1,6 +1,3 @@
-#Build FP to be picked by both system and vendor
-BUILD_FINGERPRINT := "google/coral/coral:11/RQ1A.210105.003/7005429:user/release-keys"
-
 # Apex
 ifeq ($(ENABLE_APEX), true)
 PRODUCT_PRODUCT_PROPERTIES += \
