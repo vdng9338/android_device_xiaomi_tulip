3bd860a1dd5c89756d30a7b6b5667623658e2f6f
Dyneteve <sound0020@gmail.com>
2021-03-18 07:06:03 +0600

    sdm660-common: Enable AOSP surfaceflinger * AOSP SurfaceFlinger is smoother than CAF
    Signed-off-by: Dyneteve <sound0020@gmail.com>

--- properties.mk
+++ properties.mk
@@ -366,7 +366,12 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.surface_flinger.max_frame_buffer_acquired_buffers=3 \
     ro.surface_flinger.max_virtual_display_dimension=4096 \
     ro.surface_flinger.vsync_event_phase_offset_ns=2000000 \
-    ro.surface_flinger.vsync_sf_event_phase_offset_ns=6000000
+    ro.surface_flinger.vsync_sf_event_phase_offset_ns=6000000 \
+    ro.surface_flinger.use_smart_90_for_video=true \
+    ro.surface_flinger.set_display_power_timer_ms=10000 \
+    ro.surface_flinger.set_touch_timer_ms=5000 \
+    ro.surface_flinger.set_idle_timer_ms=9000 \
+    ro.surface_flinger.wcg_composition_dataspace=143261696
 
 #Storaged
  PRODUCT_PROPERTY_OVERRIDES += \
